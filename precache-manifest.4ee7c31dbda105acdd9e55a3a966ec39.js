495bd54e97e20cee3edb4c76f895c3050f1e9e93
SeoJaeWan <52366178+SeoJaeWan@users.noreply.github.com>
2020-01-02 22:11:10 +0900

    Updates

---
+++ precache-manifest.4ee7c31dbda105acdd9e55a3a966ec39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "357fd4096de545e2f9af9a81c41062fb",
+    "url": "/Todo_2020-01-01/index.html"
+  },
+  {
+    "revision": "b8cbaf6fa4c6dee7150b",
+    "url": "/Todo_2020-01-01/static/css/main.24a5a1b1.chunk.css"
+  },
+  {
+    "revision": "abd4e65e7ef14ce16722",
+    "url": "/Todo_2020-01-01/static/js/2.7cf1e6bb.chunk.js"
+  },
+  {
+    "revision": "7e5a07cb00bc8c8b922635a3cc993e4d",
+    "url": "/Todo_2020-01-01/static/js/2.7cf1e6bb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b8cbaf6fa4c6dee7150b",
+    "url": "/Todo_2020-01-01/static/js/main.d623f39b.chunk.js"
+  },
+  {
+    "revision": "9978c04ebd2abddfbfcd",
+    "url": "/Todo_2020-01-01/static/js/runtime-main.a64bf876.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Todo_2020-01-01/precache-manifest.35645508611267da2d350ad474e5d81c.js"
+  "/Todo_2020-01-01/precache-manifest.4ee7c31dbda105acdd9e55a3a966ec39.js"
 );
 
 self.addEventListener('message', (event) => {
